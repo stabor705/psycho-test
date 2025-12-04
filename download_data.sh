ff439a52e811b0b23793885323a6a2bdc81c8a56
Stanisław Borowy <stabor@startmail.com>
2025-12-04 18:25:35 +0100

    add data downloading

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget 'https://openpsychometrics.org/tests/characters/data/SWCPQ-Features-Aggregated-Dataset-January2025.zip'
+unzip -j SWCPQ-Features-Aggregated-Dataset-January2025.zip -d data/
+rm SWCPQ-Features-Aggregated-Dataset-January2025.zip
